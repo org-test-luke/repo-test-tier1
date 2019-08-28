b7ec0969d1f4bbe6bbed478b3776b13f34971f07
luke-li-tfs <49880614+luke-li-tfs@users.noreply.github.com>
2019-08-28 15:10:42 -0700

    Update: README.md notebooks

---
+++ notebooks/Users/luke.li@thermofisher.com/Test/Temp.py
@@ -0,0 +1,75 @@
+# Databricks notebook source
+# MAGIC %sh telnet 10.0.1.97 1531 
+# MAGIC #P05.fishersci.com	CCG Oracle EBS PRD
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.1.23 1601
+# MAGIC #qadb1.fishersci.com	CCG Oracle EBS QA
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.201.50 1521
+# MAGIC #Uspgh-coredb-p1.amer.thermo.com	Hybris Prod DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.1.211 1521
+# MAGIC #P42.fishersci.com CCG Siebel PODS DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.160.15 1533
+# MAGIC #P69.fishersci.com	Fishersci WCS DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.1.185 1532
+# MAGIC #P79.fishersci.com	LPD BAAN Prod DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.130.117 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Production DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.130.118 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Production DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.130.119 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Production DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.128.17 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Stage DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.128.18 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Stage DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.128.19 1521
+# MAGIC #Uspgh-rhdb3-pscan.amer.thermo.com	Webmethod TradingNetwork DB Stage DB
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.130.115 1521
+# MAGIC #Not for PROD
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.130.116 1521
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.33.67.7 1521
+
+# COMMAND ----------
+
+# MAGIC %sh telnet 10.0.128.16 1521
